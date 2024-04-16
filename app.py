e67a01a5c922b79ed47c75aa81c8bb985be9e0f7
Puneet Bajaj <142579024+Puneet-Bajaj-IITM@users.noreply.github.com>
2024-04-16 11:17:03 +0000

    f

--- app.py
+++ app.py
@@ -1,4 +1,55 @@
-from openai import OpenAI
+import streamlit as st
+from langchain_community.document_loaders import Docx2txtLoader
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community import embeddings
+from langchain_community.llms import Ollama
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.text_splitter import CharacterTextSplitter 
+
+text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=7500, chunk_overlap=100)
+loader = Docx2txtLoader('About_the_founders.docx')
+data = loader.load()
+doc_splits = text_splitter.split_documents(data)
+
+from langchain_community.embeddings import OllamaEmbeddings
+
+vectorstore = Chroma.from_documents(
+    documents=doc_splits,
+    collection_name="rag-chroma",
+    embedding=OllamaEmbeddings(model='nomic-embed-text'),
+)
+retriever = vectorstore.as_retriever()
+
+from langchain.callbacks.base import BaseCallbackHandler
+
+class StreamingCallbackHandler(BaseCallbackHandler):
+    def __init__(self):
+        self.partial_output = ""
+
+    def on_llm_new_token(self, token, **kwargs):
+        self.partial_output += token
+        print(token, end="", flush=True)
+ 
+ # URL processing
+def process_input(question):
+    model_local = Ollama(model="llama2", callbacks=[StreamingCallbackHandler()])
+    
+    after_rag_template = """You are a College admission counsellor dedicated towards college,Answer the question based only on the following context:
+    {context}
+    Question: {question}
+    """
+    after_rag_prompt = ChatPromptTemplate.from_template(after_rag_template)
+    after_rag_chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | after_rag_prompt
+        | model_local
+        | StrOutputParser()
+    )
+    return after_rag_chain.invoke(question) 
+
 import streamlit as st
 from dotenv import load_dotenv
 import os
@@ -10,17 +61,16 @@ st.title("Streamlit Chatbot Interface")
 
 USER_AVATAR = "👤"
 BOT_AVATAR = "🤖"
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 # Ensure openai_model is initialized in session state
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    st.session_state["openai_model"] = "meta-llama/Llama-2-70b-chat-hf"
 
 
 # Load chat history from shelve file
 def load_chat_history():
     with shelve.open("chat_history") as db:
-        return db.get("messages", [])
+        return db.get("messages", []) 
 
 
 # Save chat history to shelve file
@@ -54,15 +104,12 @@ if prompt := st.chat_input("How can I help?"):
     with st.chat_message("assistant", avatar=BOT_AVATAR):
         message_placeholder = st.empty()
         full_response = ""
-        for response in client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=st.session_state["messages"],
-            stream=True,
-        ):
-            full_response += response.choices[0].delta.content or ""
+        for response in process_input(prompt):
+            full_response += response or ""
             message_placeholder.markdown(full_response + "|")
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
 
 # Save chat history after each interaction
 save_chat_history(st.session_state.messages)
+
